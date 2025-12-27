c42f1b7e18dbd86285167a94ea7dfc59b78dcfc0
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-12-27 21:20:46 +0530

    created the folder structure of frontend and landing page

---
+++ frontend/src/screens/Landing.tsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom"
+
+
+const Landing = () => {
+    const navigate = useNavigate();
+  return (
+    <div className="flex justify-center">
+        <div className="pt-8 max-w-5xl">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                <div className="flex justify-center">
+                    <img src={"/chess-board.jpeg"} className="max-w-96"></img>
+
+                </div>
+
+                <div className="pt-16">
+                    <div className="flex justify-center">
+                        <h1 className="text-4xl font-bold text-white">Play Chess Game online on the #2 Site!</h1>
+                    </div>
+
+                    <div className="mt-8 flex justify-center">
+                        <button onClick={()=>{
+                            navigate("/game")
+                        }} className="px-8 py-4 text-2xl  bg-green-500 hover:bg-green-700 text-white font-bold rounded">
+                            Play Online
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Landing
